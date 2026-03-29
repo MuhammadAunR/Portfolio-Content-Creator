83e07e61582dffbc00be5a5d6034c8ea15e28988
M Aun Rasheed <greataun786@gmail.com>
2026-03-29 09:41:49 +0500

    feat; made about section responsive

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -9,14 +9,14 @@ const AboutMe = () => {
                     <div className="h-px w-full bg-linear-to-r from-red-500 via-yellow-500 via-green-500 via-blue-500 via-indigo-500 to-purple-500"></div>
                 </div>
 
-                <p className='w-1/2 text-center'>Welcome to the cinematic realm of Jimmy, where creativity meets technology to craft visually stunning narratives. With a passion for storytelling and a keen eye for detail, I embark on a journey to transform ordinary footage into extraordinary visual experiences.</p>
+                <p className='md:w-1/2 text-center px-5'>Welcome to the cinematic realm of Jimmy, where creativity meets technology to craft visually stunning narratives. With a passion for storytelling and a keen eye for detail, I embark on a journey to transform ordinary footage into extraordinary visual experiences.</p>
 
                 <div className='space-y-5'>
                     <div className='flex flex-col items-center'>
                         <h4 className='font-semibold text-2xl text-center'>Services</h4>
                         <div className="h-px w-1/5 bg-linear-to-r from-red-500 via-yellow-500 via-green-500 via-blue-500 via-indigo-500 to-purple-500"></div>
                     </div>
-                    <ul className='flex gap-5 items-center justify-center'>
+                    <ul className='flex gap-5 items-center justify-center flex-wrap px-5'>
                         {["Cash Cow Editing", "Reels Editing", "Logo Animation", "Podcast Edit"].map((skill) => (
                             <li key={skill} className="flex items-center gap-2 text-xl">
                                 <div className="h-3 w-3 rounded-full bg-linear-to-r from-red-500 via-yellow-500 via-green-500 via-blue-500 via-indigo-500 to-purple-500 shrink-0"></div>
@@ -31,7 +31,7 @@ const AboutMe = () => {
                         <h4 className='font-semibold text-2xl text-center'>Tools</h4>
                         <div className="h-px w-1/4 bg-linear-to-r from-red-500 via-yellow-500 via-green-500 via-blue-500 via-indigo-500 to-purple-500"></div>
                     </div>
-                    <ul className='flex gap-5 items-center justify-center'>
+                    <ul className='flex gap-5 items-center justify-center flex-wrap px-5'>
                         <li className='hover:scale-110 transition-transform ease-linear duration-300'>   <img src="https://skillicons.dev/icons?i=ae" /></li>
                         <li className='hover:scale-110 transition-transform ease-linear duration-300'>   <img src="https://skillicons.dev/icons?i=ps" /></li>
                         <li className='hover:scale-110 transition-transform ease-linear duration-300'>   <img src="https://skillicons.dev/icons?i=pr" /></li>
